65db81cdcc823c3a690004735fa4e3ca95e620fb
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-21 16:06:21 +0100

    Implement ping button handler.

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -2,7 +2,22 @@ Ext.define('Sencha.controller.Main', {
     extend: 'Ext.app.Controller',
 
     config: {
-        refs: {},
-        control: {}
+        refs: {
+            homeTabButton: 'tabbar button[title=home]',
+            starButton:    'button[action=pingHomeBadge]'
+        },
+        control: {
+            starButton: {
+                tap: 'incrementHomeBadge'
+            }
+        }
+    },
+
+    incrementHomeBadge: function() {
+        var hometab = this.getHomeTabButton(),
+            badgenumber = parseInt(hometab.getBadgeText()),
+            nextnumber = isNaN(badgenumber) ? 1 : badgenumber+1;
+
+        hometab.setBadgeText(nextnumber);
     }
 });
