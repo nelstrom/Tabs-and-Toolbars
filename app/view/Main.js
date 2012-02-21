cbe8dba80fe48bcdaa41a54f8a045cff6796c112
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-21 10:53:07 +0100

    Remove 'Hello, World!' text from Main view.

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,10 +1,5 @@
 Ext.define('Sencha.view.Main', {
     extend: 'Ext.Panel',
 
-    config: {
-        fullscreen: true,
-        styleHtmlContent: true,
-        scrollable: 'vertical',
-        html: 'Hello, world!'
-    }
+    config: {}
 });
