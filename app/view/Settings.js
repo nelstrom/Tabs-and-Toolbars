a79ec8b5081c0c47f542ee8578818b2b192ae7b1
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-21 11:57:38 +0100

    Make Settings card show a login form.

--- app/view/Settings.js
+++ app/view/Settings.js
@@ -1,9 +1,34 @@
 Ext.define('Sencha.view.Settings', {
-    extend: 'Ext.Panel',
+    extend: 'Ext.form.FormPanel',
     xtype: 'settingscard',
+    requires: [
+        'Ext.form.FieldSet',
+        'Ext.field.Password'
+    ],
 
     config: {
         iconCls: 'settings',
         title: 'Settings',
+        items: [
+            {
+                xtype: 'fieldset',
+                title: 'Details',
+                items: [
+                    {
+                        xtype: 'textfield',
+                        name : 'name',
+                        label: 'Username'
+                    },{
+                        xtype: 'passwordfield',
+                        name : 'password',
+                        label: 'Password'
+                    }
+                ],
+            },{
+                xtype:  'button',
+                text:   'save',
+                ui:     'confirm'
+            }
+        ]
     }
 });
